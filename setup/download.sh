b033f8a397234904ae16bb8da7d0ea0edf8aafb9
Oliver Moss <moss.oliver@outlook.com>
2025-01-25 16:49:10 +1030

    Tweaks to download script

--- setup/download.sh
+++ setup/download.sh
@@ -1,4 +1,13 @@
 pacman --needed -S git
 cd ~
-git clone https://github.com/moss-oliver/cfg tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+git clone https://github.com/moss-oliver/cfg tmp
+mv tmp/.git .
+rm -rf tmp
+git reset --hard
+
+chmod +x ./setup/setup.sh
+chmod +x ./setup/install_pacman.sh
+chmod +x ./setup/install_paru.sh
+chmod +x ./setup/config_kde.sh
+
 ./setup/setup.sh
